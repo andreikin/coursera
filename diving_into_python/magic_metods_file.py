96c404ca253432393a19b62028fcd754dfab5a1b
andreikin <andreikin@mail.ru>
2020-12-07 16:03:26 +0300

    making magic metods

--- diving_into_python/magic_metods_file.py
+++ diving_into_python/magic_metods_file.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 
 class File:
@@ -16,6 +17,7 @@ class File:
     полученной с помощью функции tempfile.gettempdir. Для получения нового пути можно 
     использовать os.path.join."""
     def __add__(self, other):
+        sumfile_path = os.path.join(tempfile.gettempdir(), 'storage.data')
         pass
 
     """- возвращать в качестве строкового представления объекта класса File полный путь до файла"""
@@ -44,9 +46,42 @@ class File:
 
 
 
-fl = File('test.txt')
-print(fl.is_path_exists())
+# fl = File('test.txt')
+# print(fl.is_path_exists())
+#
+# fl.write('gggggggg')
+#
+# print(fl.read())
 
-#fl.write('gggggggg')
 
-print(fl.read())
+path_to_file = 'some_filename'
+print(os.path.exists(path_to_file))
+#False
+file_obj = File(path_to_file)
+print(os.path.exists(path_to_file))
+# #True
+#print(file_obj.read())
+# #''
+#file_obj.write('some text')
+
+print(file_obj.read())
+# 'some text'
+file_obj.write('other text')
+
+file_obj.read()
+# #'other text'
+# file_obj_1 = File(path_to_file + '_1')
+# file_obj_2 = File(path_to_file + '_2')
+# file_obj_1.write('line 1\n')
+# #7
+# file_obj_2.write('line 2\n')
+# #7
+# new_file_obj = file_obj_1 + file_obj_2
+# isinstance(new_file_obj, File)
+# #True
+# print(new_file_obj)
+# #C:\Users\Media\AppData\Local\Temp\71b9e7b695f64d85a7488f07f2bc051c
+# for line in new_file_obj:
+#     print(ascii(line))
+# #'line 1\n'
+# #'line 2\n'
